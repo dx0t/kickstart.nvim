5eb313f1f3f479c280cfc7f1d4d266c2bb80228e
dx0t <trzeciokdenis@gmail.com>
2026-03-13 12:46:01 +0100

    change default language

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.cmd 'language en_US'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
