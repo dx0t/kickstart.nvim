139061584e37389e6dc5b5c3d354e0a8585465e3
dx0t <trzeciokdenis@gmail.com>
2026-03-24 20:42:12 +0100

    Added omnisharp + enabled kickstart plugins

--- init.lua
+++ init.lua
@@ -460,6 +460,7 @@ require('lazy').setup({
       ---@type table<string, vim.lsp.Config>
       local servers = {
         -- clangd = {},
+        omnisharp = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
@@ -792,11 +793,11 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   --require 'kickstart.plugins.debug',
-  --require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.indent_line',
   --require 'kickstart.plugins.lint',
-  --require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   --require 'kickstart.plugins.neo-tree',
-  --require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
