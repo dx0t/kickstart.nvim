6f9fff7e3bdb088913e46d329cfcbce43b6e1174
dx0t <trzeciokdenis@gmail.com>
2026-04-21 20:34:33 +0200

    changed default diff fillchar

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -8,5 +8,4 @@ return {
     { '<leader>gD', '<cmd>DiffviewClose<cr>', desc = 'Diffview Close' },
     { '<leader>gh', '<cmd>DiffviewFileHistory %<cr>', desc = 'Diffview File History' },
   },
-  opts = {},
 }
