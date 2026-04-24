adfc7d524ae0ab6bf2643dbdc3dca763694059c9
Denis Trzeciok <denis.trzeciok@intechnity.com>
2026-04-24 15:05:18 +0200

    fix diffview

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -3,6 +3,14 @@ return {
   lazy = true,
   cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewFileHistory' },
   dependencies = { 'nvim-lua/plenary.nvim' },
+  init = function()
+    vim.api.nvim_create_autocmd('BufNew', {
+      group = vim.api.nvim_create_augroup('custom-diffview-noswapfile', { clear = true }),
+      callback = function(args)
+        if not vim.bo[args.buf].buflisted then vim.bo[args.buf].swapfile = false end
+      end,
+    })
+  end,
   keys = {
     { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = 'Diffview Open' },
     { '<leader>gD', '<cmd>DiffviewClose<cr>', desc = 'Diffview Close' },
