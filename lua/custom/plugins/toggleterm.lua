486cca2d32725b0b70c9b7ebbaf3fe7add2a3693
Denis Trzeciok <denis.trzeciok@intechnity.com>
2026-06-30 15:42:35 +0200

    new shortcuts for toggle term

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,9 +1,15 @@
 return {
   'akinsho/toggleterm.nvim',
   version = '*',
-  cmd = 'ToggleTerm',
+  cmd = { 'ToggleTerm', 'ToggleTermToggleAll', 'TermSelect' },
   keys = {
     { '<leader>tt', '<cmd>ToggleTerm<cr>', desc = 'Toggle terminal' },
+    { '<leader>t1', '<cmd>1ToggleTerm<cr>', desc = 'Toggle terminal 1' },
+    { '<leader>t2', '<cmd>2ToggleTerm<cr>', desc = 'Toggle terminal 2' },
+    { '<leader>t3', '<cmd>3ToggleTerm<cr>', desc = 'Toggle terminal 3' },
+    { '<leader>t4', '<cmd>4ToggleTerm<cr>', desc = 'Toggle terminal 4' },
+    { '<leader>ta', '<cmd>ToggleTermToggleAll<cr>', desc = 'Toggle all terminals' },
+    { '<leader>ts', '<cmd>TermSelect<cr>', desc = 'Select terminal' },
   },
   opts = {
     open_mapping = [[<c-\>]],
